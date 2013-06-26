e5955b65af74df1854620692fc4881645f88061b
Antonio F. Lancho <onioman@gmail.com>
2013-06-26 19:49:15 +0900

    First test case.

---
+++ Rectangles/src/main/java/com/onioman/katas/Rectangle.java
@@ -0,0 +1,11 @@
+package com.onioman.katas;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: onio
+ * Date: 6/26/13
+ * Time: 7:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Rectangle {
+}
---
+++ Rectangles/src/test/java/com/onioman/katas/RectangleTest.java
@@ -0,0 +1,31 @@
+package com.onioman.katas;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: onio
+ * Date: 6/26/13
+ * Time: 7:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RectangleTest {
+    @Test
+    public void everythingInPlace() {
+        assertTrue(true);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
