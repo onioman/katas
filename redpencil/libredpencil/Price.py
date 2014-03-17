b80a3b9a6b49675d549b85445f3b2edcec9e679b
Antonio F. Lancho <onioman@gmail.com>
2014-03-17 22:32:40 +0900

    First reductions of number of members.

--- redpencil/libredpencil/Price.py
+++ redpencil/libredpencil/Price.py
@@ -3,77 +3,90 @@ from twisted.internet import reactor
 SECS_IN_30DAYS=60*60*24*30
 
 class PriceState(object):
-	def __init__(self):
-		pass
-
-class Price:
-	def __init__(self, value, reactor=reactor):
+	def __init__(self, reactor):
 		self._stable = False
 		self._inPromotion = False
 		self._reactor = reactor
-		self._value = value
 		self._stabilityTimer = None
 		self._promotionTimer = None
 		self._promotionValue = 0
 
 		self._startStabilityTimer()
 
-	def stable(self):
+	def makeUnstable(self):
+		self._stable = False
+		if not self._inPromotion:
+			self._startStabilityTimer()
+
+	def isStable(self):
 		return self._stable
 
-	def setValue(self, value):
+	def isInPromotion(self):
+		return self._inPromotion
+
+	def endPromotion(self):
+		if self._promotionTimer and self._promotionTimer.active():
+			self._promotionTimer.cancel()
+		days = self._reactor.seconds()/24/60/60
+		self._promotionTimer = None
+		self._stable = False
+		self._inPromotion = False
 		self._startStabilityTimer()
 
-		diff = self._calculateDiffInPercentage(value)
+	def startPromotion(self, value):
+		self._promotionValue = value
+		self._inPromotion = True
+		self._promotionTimer = self._reactor.callLater(SECS_IN_30DAYS, self.endPromotion)
 
-		if value > self._value:
-			self._unsetPromotion()
-
-		if diff >= 5 and diff <= 30 and self._stable:
-			self._setPromotion()
-
-		if diff > 30 and self._inPromotion:
-			self._unsetPromotion()
-
-		self._value = value
-
-		self._stable = False
-
-	def _calculateDiffInPercentage(self, value):
-		current = self._value
-		if self._inPromotion:
-			current = self._promotionValue
-		diff = current - value	
-		diff_percent = float(diff) / current * 100.0
-		return diff_percent
+	def promotionValue(self):
+		return self._promotionValue
 
 	def _startStabilityTimer(self):
 		self._cancelStabilityTimer()
 		self._stabilityTimer = self._reactor.callLater(\
 			SECS_IN_30DAYS, self._becomeStable)
-	
-	def _becomeStable(self):
-		self._stable = True
-		self._stabilityTimer = None
 
 	def _cancelStabilityTimer(self):
 		if self._stabilityTimer:
 			self._stabilityTimer.cancel()
 
-	def _setPromotion(self):
-		self._promotionValue = self._value
-		self._inPromotion = True
-		self._promotionTimer = self._reactor.callLater(SECS_IN_30DAYS, self._unsetPromotion)
+	def _becomeStable(self):
+		self._stable = True
+		self._stabilityTimer = None
 
-	def _unsetPromotion(self):
-		if self._promotionTimer and self._promotionTimer.active():
-			self._promotionTimer.cancel()
-		self._promotionTimer = None
-		self._stable = False
-		self._inPromotion = False
+class Price:
+	def __init__(self, value, reactor=reactor):
+		self._value = value
+		self._state = PriceState(reactor)
+
+	def stable(self):
+		return self._state.isStable()
+
+	def setValue(self, value):
+		diff = self._calculateDiffInPercentage(value)
+
+		if value > self._value:
+			self._state.endPromotion()
+
+		if diff >= 5 and diff <= 30 and self._state.isStable():
+			self._state.startPromotion(self._value)
+
+		if diff > 30 and self._state.isInPromotion():
+			self._state.endPromotion()
+
+		self._state.makeUnstable()
+		self._value = value
+
+	def _calculateDiffInPercentage(self, value):
+		current = self._value
+		if self._state.isInPromotion():
+			current = self._state.promotionValue()
+		diff = current - value	
+		diff_percent = float(diff) / current * 100.0
+		return diff_percent
 
 	def value(self):
 		return self._value
 	
 	def inPromotion(self):
-		return self._inPromotion
+		return self._state.isInPromotion()
