e4ee860f10870239f889e91e964c3ed4ee6caeb2
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-09-25 15:21:48 +0900

    Allocate final solution, gen test inspired by Sebas.

--- allocate/liballocate/Allocate.py
+++ allocate/liballocate/Allocate.py
@@ -1,17 +1,23 @@
 import math
 
 def allocate(quantity, weights):
-	if not weights: return weights
+    if not weights: return weights
 
-	def _allocate_next(total, remains, ws, acum):
-		if len(ws) == 1:
-			acum.append(remains)
-			return acum
-		else:
-			w = ws.pop(0)
-			p = w / float(total) if total else 0
-			n = math.ceil(remains * p)
-			acum.append(n)
-			return _allocate_next(total-w, remains-n, ws, acum)
+    # because of this ordering under same conditions, the last gets more
+    indexed_weights = sorted([(weights[i], i) for i in range(len(weights))], reverse=True)
+    allocation      = [0 for _ in range(len(weights))]
+    total_weight    = sum(weights)
 
-	return _allocate_next(sum(weights), quantity, weights, [])
+    def _allocate_next(total, remains, ws, allocation):
+        if len(ws) == 1:
+            (_,i) = ws.pop(0)
+            allocation[i] = remains
+            return allocation
+        else:
+            (w,i) = ws.pop(0)
+            p     = w / float(total) if total else 0
+            n     = math.ceil(remains * p)
+            allocation[i] = n
+            return _allocate_next(total-w, remains-n, ws, allocation)
+
+    return _allocate_next(total_weight, quantity, indexed_weights, allocation)
--- allocate/test/AllocateTest.py
+++ allocate/test/AllocateTest.py
@@ -1,5 +1,6 @@
 from twisted.trial import unittest
 from twisted.internet.task import Clock
+import math
 
 from liballocate.Allocate import allocate
 
@@ -17,19 +18,58 @@ class AllocateTest(unittest.TestCase):
 		self.assertEqual([10, 0, 0], allocate(10, [1, 0, 0]))
 
 	def test_nonDivisible(self):
-		self.assertEqual([4, 3, 3], allocate(10, [1, 1, 1]))
+		self.assertEqual([3, 3, 4], allocate(10, [1, 1, 1]))
 
-	def test_firstPreference(self):
-		self.assertEqual([4, 3], allocate(7, [50, 50]))
+	def test_lastGetsMore(self):
+		self.assertEqual([3,4], allocate(7, [50, 50]))
 
 	def test_almostSame(self):
 		self.assertEqual([3, 4], allocate(7, [50, 51]))
 
-	def test_justOnTest(self):
-		self.assertEqual([5,4,1], allocate(10, [4,4,1]))
+	def test_justTest(self):
+		self.assertEqual([4,5,1], allocate(10, [4,4,1]))
+
+        def test_genAllocationAndWeightsSameLength(self):
+            self.assertEqual(len(self.weights), len(self.allocation))
+
+        def test_genExactQuantityAllocated(self):
+            self.assertEqual(self.quantity, sum(self.allocation))
+
+        def test_genProportionalAllocation(self):
+            def _error(expected, got, index):
+                msg=str( [(self.weights[i],self.allocation[i]) for i in range(len(self.allocation))])
+                return "index %s -> (expected) %s != %s (got) -> %s"%(index, expected, got, msg)
+
+            total_weight =  sum(self.weights)
+            index = 0
+            for weight in self.weights:
+                p = weight / float(total_weight)
+                expected = p * self.quantity
+                got = self.allocation[index]
+                self.assertLessEqual(abs(got - expected), 2, msg=_error(expected, got, index))
+                index += 1
+
+        def test_genFair(self):
+            for i in range(len(self.weights)):
+                for j in range(len(self.weights)):
+                    if self.weights[i] > self.weights[j]:
+                        self.assertGreaterEqual(self.allocation[i], self.allocation[j])
 
 	def setUp(self):
-		pass
+            self.weights = self._random_list()
+            self.quantity = self._random_quantity()
+
+            self.allocation = allocate(self.quantity, self.weights)
+
+        def _random_list(self):
+            import random
+            lenght = random.randint(0, 100)
+            return random.sample(range(1000), lenght)
+
+        def _random_quantity(self):
+            import random
+            return random.randint(0, 1000)
+
 
 	def tearDown(self):
 		pass
